fcbf15d4942ae600e3ef40882fd3773958cf2f8b
Manoel Dantas <manoel.lobo@gmail.com>
2020-09-06 14:47:17 -0300

    feat: change availability

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -87,7 +87,6 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
     setEditingFood(food);
 
     toggleEditModal();
